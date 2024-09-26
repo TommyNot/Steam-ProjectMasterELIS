fcd4dd809f5e02594653b276333ea558a97b5c0c
TommyNot <tommasomaglios@gmail.com>
2024-09-26 15:29:13 +0200

    Update Recensioni

--- main/java/org/elis/model/Recensione.java
+++ main/java/org/elis/model/Recensione.java
@@ -1,6 +1,13 @@
 package org.elis.model;
 
+import java.time.LocalDateTime;
+
 public class Recensione {
+	private long id;
+	private LocalDateTime data_creazione;
+	private LocalDateTime data_ultima_modifica;
+	private int voto;
+	private String testo;
 	private Gioco gioco;
 	private Utente utente;
 	
