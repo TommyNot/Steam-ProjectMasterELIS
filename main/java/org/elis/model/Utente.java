8f617f292151429efe9d5c088abdf27ea485a13b
TommyNot <tommasomaglios@gmail.com>
2024-09-26 12:30:25 +0200

    Update Utente.java
    cambiato costruttore di classe Utente model

--- main/java/org/elis/model/Utente.java
+++ main/java/org/elis/model/Utente.java
@@ -17,15 +17,14 @@ public class Utente {
 	private List<Recensione> rencensioni;
 	
 	
-	public Utente(Ruolo ruolo, String username, String email, String password) {
-		
-		this.ruolo = ruolo;
-		
-		this.username = username;
-		this.email = email;
-		this.password = password;
+	public Utente(Ruolo ruolo, String username, String email, String password, LocalDateTime data_creazione, LocalDateTime data_modifica) {
+	    this.ruolo = ruolo;
+	    this.username = username;
+	    this.email = email;
+	    this.password = password;
+	    this.data_creazione = data_creazione;
+	    this.data_modifica = data_modifica;
 	}
-
 
 	public Utente() {
 		
@@ -79,6 +78,26 @@ public class Utente {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+
+	public LocalDateTime getData_creazione() {
+		return data_creazione;
+	}
+
+
+	public void setData_creazione(LocalDateTime data_creazione) {
+		this.data_creazione = data_creazione;
+	}
+
+
+	public LocalDateTime getData_modifica() {
+		return data_modifica;
+	}
+
+
+	public void setData_modifica(LocalDateTime data_modifica) {
+		this.data_modifica = data_modifica;
 	}
 	
 	
