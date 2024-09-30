ef99eb284a154608994e74b5ac6059417bc20b9d
fffovet <capa898@gmail.com>
2024-09-30 11:50:13 +0200

    genere

--- main/java/org/elis/jdbc/GenereDaoJDBC.java
+++ main/java/org/elis/jdbc/GenereDaoJDBC.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.elis.dao.GenereDao;
 import org.elis.model.Genere;
+import org.elis.model.Offerta;
 
 public class GenereDaoJDBC implements GenereDao{
 
@@ -63,21 +67,99 @@ private static GenereDaoJDBC instance;
 
 	@Override
 	public List<Genere> findAll() {
-		// TODO Auto-generated method stub
+		List<Genere> genere = new ArrayList<>();
+		String query="SELECT * FROM GENERE";
+		
+		try(
+				Connection c=JdbcDaoFactory.getConnection();
+				PreparedStatement ps=c.prepareStatement(query);
+				ResultSet rs=ps.executeQuery();
+				){
+			while(rs.next()) {
+				Genere g=new Genere();
+				String nome=rs.getString("nome");
+				Timestamp dataCreazione=rs.getTimestamp("data_creazione");
+                LocalDateTime data_creazione = dataCreazione != null ? dataCreazione.toLocalDateTime() : null;
+                Timestamp dataModifica=rs.getTimestamp("data_ultima_modifica");
+                LocalDateTime data_modifica= dataModifica !=null ? dataModifica.toLocalDateTime():null;
+                Offerta offerta=((Genere) rs).getOfferta();     
+                
+                g.setNome(nome);
+                g.setData_creazione(data_creazione);
+                g.setData_ultima_modifica(data_modifica);
+                g.setOfferta(offerta);
+                
+                genere.add(g);
+				
+			}
+					
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public Genere findByName(String nome) {
-		// TODO Auto-generated method stub
-		return null;
+	    String query = "SELECT * FROM genere WHERE nome = ?";
+	    
+	    try (
+	        Connection c = JdbcDaoFactory.getConnection(); 
+	        PreparedStatement ps = c.prepareStatement(query)
+	    ) {
+	        ps.setString(1, nome);
+	        
+	        try (ResultSet rs = ps.executeQuery()) {
+	            if (rs.next()) {
+	                Genere genere = new Genere();
+	                genere.setId(rs.getLong("id"));
+	                genere.setNome(rs.getString("nome"));
+	                return genere;
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	    return null;
 	}
+
 
 	@Override
 	public Genere selectById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	    String query = "SELECT * FROM genere WHERE id = ?";
+	    
+	    try (
+	        Connection c = JdbcDaoFactory.getConnection(); 
+	        PreparedStatement ps = c.prepareStatement(query)
+	    ) {
+
+	        ps.setLong(1, id);
+	        
+	        try (ResultSet rs = ps.executeQuery()) {
+	            if (rs.next()) {
+	                Genere genere = new Genere();
+	                genere.setId(rs.getLong("id"));   
+	                genere.setNome(rs.getString("nome"));  
+	                return genere;
+	            }
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	    return null;
 	}
+
 
 	@Override
 	public Genere deleteByName(String nome) {
