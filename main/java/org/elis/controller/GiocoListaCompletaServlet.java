2de5d4b55f143cb70c3ad9c753d64c7532364d03
TommyNot <tommasomaglios@gmail.com>
2024-10-01 15:29:15 +0200

    Nuova Servlet GiocoListaCompleta

---
+++ main/java/org/elis/controller/GiocoListaCompletaServlet.java
@@ -0,0 +1,55 @@
+package org.elis.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import org.elis.businesslogic.BusinessLogic;
+import org.elis.model.Gioco;
+
+/**
+ * Servlet implementation class GiocoListaCompletaServlet
+ */
+public class GiocoListaCompletaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GiocoListaCompletaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	      
+        List<Gioco> listaGiochi = BusinessLogic.VisualizzaTuttiGiochi();
+        
+        if (listaGiochi == null || listaGiochi.isEmpty()) {
+            request.setAttribute("errorMessage", "Nessun gioco disponibile.");
+            request.getRequestDispatcher("WEB-INF/private-jsp/error.jsp").forward(request, response);
+            return;
+        }
+
+
+      
+        	
+         request.setAttribute("giochi", listaGiochi);
+         request.getRequestDispatcher("WEB-INF/private-jsp/GiochiLista.jsp").forward(request, response);
+        
+        
+
+        
+       
+	}
+
+	
+
+}
