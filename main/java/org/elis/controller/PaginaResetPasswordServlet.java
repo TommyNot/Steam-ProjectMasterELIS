c56d9d4b65e70bd489a916a85a36069db7cddd1c
TommyNot <tommasomaglios@gmail.com>
2024-10-02 17:46:57 +0200

    Update

--- main/java/org/elis/controller/PaginaResetPasswordServlet.java
+++ main/java/org/elis/controller/PaginaResetPasswordServlet.java
@@ -27,17 +27,7 @@ public class PaginaResetPasswordServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
 		if (session == null || session.getAttribute("userId") == null) {
