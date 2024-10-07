695db8db4ba7a3821cd6f242b43f2984fe045a86
fffovet <capa898@gmail.com>
2024-10-07 09:45:02 +0200

    Update PaginaResetPasswordServlet.java

--- main/java/org/elis/controller/PaginaResetPasswordServlet.java
+++ main/java/org/elis/controller/PaginaResetPasswordServlet.java
@@ -30,12 +30,13 @@ public class PaginaResetPasswordServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
-		if (session == null || session.getAttribute("userId") == null) {
+		if (session == null || session.getAttribute("utenteLoggato") == null) {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 1");
 			return;
 		}
 
-		long idUtente = (Long) session.getAttribute("userId");
+		long idUtente = (Long) session.getAttribute("utenteLogato");
 
 		String passwordVecchia = request.getParameter("passwordVecchia");
 		String passwordNuova = request.getParameter("passwordNuova");
@@ -44,27 +45,32 @@ public class PaginaResetPasswordServlet extends HttpServlet {
 		if (passwordVecchia == null || passwordNuova == null || passwordConferma == null ||
 			passwordVecchia.isEmpty() || passwordNuova.isEmpty() || passwordConferma.isEmpty()) {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 2");
 			return;
 		}
 		
 		if (passwordVecchia.equals(passwordNuova)) {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 3");
 			return;
 		}
 		
 		if (!passwordNuova.equals(passwordConferma)) {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 4");
 			return;
 		}
 
 		Utente utente = BusinessLogic.UtenteFindById(idUtente);
 		if (utente == null) {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 5");
 			return;
 		}
 
 		if (!utente.getPassword().equals(passwordVecchia)) {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 6");
 			return;
 		}
 
@@ -73,6 +79,7 @@ public class PaginaResetPasswordServlet extends HttpServlet {
 			response.getWriter().write("Password aggiornata con successo!");
 		} else {
 			request.getRequestDispatcher("public-jsp/error.jsp").forward(request, response);
+			System.out.println("errore 7");
 		}
 		 request.getRequestDispatcher("WEB-INF/private-jsp/DashboardUtente.jsp").forward(request, response);
 	}
