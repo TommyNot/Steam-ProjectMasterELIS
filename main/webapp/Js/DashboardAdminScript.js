f8b36f9a97a8d3e73fdd04927f7387c2ac15e069
fffovet <capa898@gmail.com>
2024-10-11 14:55:20 +0200

    Update DashboardAdminScript.js

--- main/webapp/Js/DashboardAdminScript.js
+++ main/webapp/Js/DashboardAdminScript.js
@@ -69,7 +69,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
        event.preventDefault();
 	   
        const username = document.getElementById('username1').value;  
-       fetch('/SteamProject/UtenteFindByNameServlet', {
+       fetch('http://localhost:8080/Steamproject//UtenteFindByNameServlet', {
            method: 'POST',
            headers: {
                'Content-Type': 'application/x-www-form-urlencoded'
