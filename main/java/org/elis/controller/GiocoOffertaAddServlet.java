47fafebb9e902053f5ab1894a43aca1c3038ea47
LeoSanso <sansoo1927@gmail.com>
2024-10-21 14:42:36 +0200

    fix
    jpa addgenereofferta

--- main/java/org/elis/controller/GiocoOffertaAddServlet.java
+++ main/java/org/elis/controller/GiocoOffertaAddServlet.java
@@ -84,7 +84,7 @@ public class GiocoOffertaAddServlet extends HttpServlet {
 
         if (giocoOfferta == null || giocoOfferta.isEmpty()) {
             request.setAttribute("Error", "Nessun gioco trovato per l'offerta selezionata.");
-            request.getRequestDispatcher("public-jsp/DashboardUtente.jsp").forward(request, response);
+            request.getRequestDispatcher("public-jsp/DashboardAdmin.jsp").forward(request, response);
         } else {
             response.getWriter().write("Offerta associata con successo.");
         }
